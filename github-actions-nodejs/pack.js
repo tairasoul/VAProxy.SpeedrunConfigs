c39ce3ec6693c2529a09340e2241ee97f75b6f14
taira <100938045+tairasoul@users.noreply.github.com>
2023-12-29 14:54:44 +0200

    dont need to branch check anymore

--- github-actions-nodejs/pack.js
+++ github-actions-nodejs/pack.js
@@ -2,28 +2,24 @@ const adm = require("adm-zip");
 const fs = require("fs");
 const path = require("path");
 
-const branch = process.env.GITHUB_REF.split('/').slice(-1)[0];
-
-if (branch == "main") {
-    const dir = path.dirname(__dirname);
-    const directories = fs.readdirSync(dir);
-    const sorted = [];
-    const blacklist = ["README.md", "github-actions-nodejs", ".git", ".github", "node_modules", "package.json", "package-lock.json"];
-    for (const sort of directories) {
-        if (!blacklist.includes(sort)) {
-            sorted.push(sort);
-        }
+const dir = path.dirname(__dirname);
+const directories = fs.readdirSync(dir);
+const sorted = [];
+const blacklist = ["README.md", "github-actions-nodejs", ".git", ".github", "node_modules", "package.json", "package-lock.json"];
+for (const sort of directories) {
+    if (!blacklist.includes(sort)) {
+        sorted.push(sort);
     }
-    for (const directory of sorted) {
-        const builds = fs.readdirSync(`${dir}/${directory}`).filter((v) => v != "Builds");
-        for (const build of builds) {
-            const files = fs.readdirSync(`${dir}/${directory}/${build}`);
-            const zip = new adm();
-            for (const file of files) {
-                zip.addLocalFile(`${dir}/${directory}/${build}/${file}`, undefined);
-            }
-            if (!fs.existsSync(`${dir}/${directory}/Builds`)) fs.mkdirSync(`${dir}/${directory}/Builds`);
-            zip.writeZip(`${dir}/${directory}/Builds/${build}.zip`);
+}
+for (const directory of sorted) {
+    const builds = fs.readdirSync(`${dir}/${directory}`).filter((v) => v != "Builds");
+    for (const build of builds) {
+        const files = fs.readdirSync(`${dir}/${directory}/${build}`);
+        const zip = new adm();
+        for (const file of files) {
+            zip.addLocalFile(`${dir}/${directory}/${build}/${file}`, undefined);
         }
+        if (!fs.existsSync(`${dir}/${directory}/Builds`)) fs.mkdirSync(`${dir}/${directory}/Builds`);
+        zip.writeZip(`${dir}/${directory}/Builds/${build}.zip`);
     }
 }
